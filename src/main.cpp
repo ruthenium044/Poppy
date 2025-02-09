27bed110dd0f463387b6bc407c59fabd20c72bb3
deadmadman <rutaspk@gmail.com>
2025-02-09 16:51:10 +0100

    set up, create and link shjaders

---
+++ src/main.cpp
@@ -0,0 +1,186 @@
+#include <SDL3/SDL.h>
+#include <glad/glad.h>
+#include <iostream>
+
+const char* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"}\n\0";
+
+static void compileShader(unsigned int shader, const char* shaderSource)
+{
+	glShaderSource(shader, 1, &shaderSource, NULL);
+	glCompileShader(shader);
+
+	int  success;
+	char infoLog[512];
+	glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+
+	if (!success)
+	{
+		glGetShaderInfoLog(shader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+}
+
+unsigned int createAndLinkProgram( unsigned int shaders[], int count)
+{
+	unsigned int shaderProgram = glCreateProgram();
+
+	for (int i = 0; i < count; i++)
+	{
+		glAttachShader(shaderProgram, shaders[i]);
+	}
+	glLinkProgram(shaderProgram);
+
+	int  success;
+	char infoLog[512];
+	glGetShaderiv(shaderProgram, GL_COMPILE_STATUS, &success);
+
+	if (!success) {
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	for (int i = 0; i < count; i++)
+	{
+		glDeleteShader(shaders[i]);
+	}
+
+	return shaderProgram;
+}
+
+static void setUpDrawTriangle()
+{
+	float vertices[] = 
+	{
+		-0.5f, -0.5f, 0.0f,
+		0.5f, -0.5f, 0.0f,
+		0.0f,  0.5f, 0.0f
+	};
+
+	//init?
+	unsigned int VBO;
+	glGenBuffers(1, &VBO);
+
+	//bind buffer
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+	//copy data to buffer
+	//GL_STREAM_DRAW: the data is set only once and used by the GPU at most a few times.
+	//GL_STATIC_DRAW : the data is set only once and used many times.
+	//GL_DYNAMIC_DRAW : the data is changed a lot and used many times.
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	//Create and compile shaders
+	unsigned int vertexShader = glCreateShader( GL_VERTEX_SHADER );
+	compileShader(vertexShader, vertexShaderSource);
+
+	unsigned int fragmentShader = glCreateShader( GL_FRAGMENT_SHADER );
+	compileShader(fragmentShader, fragmentShaderSource);
+
+	//Create and link shader programs
+	unsigned int shaders[] = { vertexShader, fragmentShader };
+	unsigned int shaderProgram = createAndLinkProgram(shaders, SDL_arraysize(shaders));
+
+	glUseProgram(shaderProgram);
+}
+
+int main()
+{
+	// Initialize SDL
+	if (SDL_Init(SDL_INIT_VIDEO) == false) 
+	{
+		std::cerr << "SDL_Init Error: " << SDL_GetError() << std::endl;
+		return -1;
+	}
+
+	// Set OpenGL attributes
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 4);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+
+	int windowWidth = 800;
+	int windowHeight = 600;
+
+	// Create an SDL window
+	SDL_Window* window = SDL_CreateWindow("Close it.", windowWidth, windowHeight, SDL_WINDOW_OPENGL);
+	if (window == nullptr)
+	{
+		std::cerr << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
+		SDL_Quit();
+		return -1;
+	}
+
+	// Create an OpenGL context
+	SDL_GLContext glContext = SDL_GL_CreateContext(window);
+	if (glContext == nullptr)
+	{
+		std::cerr << "SDL_GL_CreateContext Error: " << SDL_GetError() << std::endl;
+		SDL_Quit();
+		return -1;
+	}
+
+	// Initialize GLAD
+	if (!gladLoadGLLoader((GLADloadproc)SDL_GL_GetProcAddress))
+	{
+		std::cerr << "Failed to initialize GLAD" << std::endl;
+		SDL_Quit();
+		return -1;
+	}
+
+	glViewport(0, 0, windowWidth, windowHeight);
+
+	// Main loop flag
+	bool quit = false;
+
+	// Event handler
+	SDL_Event e;
+
+	while (!quit)
+	{
+		while (SDL_PollEvent(&e) != 0)
+		{
+			if (e.type == SDL_EVENT_QUIT)
+			{
+				quit = true;
+			}
+
+			if (e.type == SDL_EVENT_WINDOW_RESIZED)
+			{
+				glViewport(0, 0, e.window.data1, e.window.data2);
+			}
+
+			if (e.type == SDL_EVENT_KEY_DOWN)
+			{
+				if (e.key.key == SDLK_ESCAPE)
+				{
+					quit = true;
+				}
+			}
+		}
+
+		// Clear screen
+		glClearColor(0.3f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// Update window
+		SDL_GL_SwapWindow(window);
+	}
+
+	// Clean up
+	SDL_GL_DestroyContext(glContext);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+
+	return 0;
+}
