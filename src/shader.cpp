3730a6cf443397c779efd9f230af9028ba9f8f2b
Ruthenium <ruthenium@Rutheniums-MacBook-Air-2.local>
2025-10-15 12:26:43 +0200

    restructure

--- src/shader.cpp
+++ src/shader.cpp
@@ -1,6 +1,242 @@
 #include "shader.h"
 
 #include "SDL3/SDL_stdinc.h"
+#include <SDL3/SDL.h>
+#include <SDL3_image/SDL_image.h>
+#include <glad/glad.h>
+#include <iostream>
+#include <filesystem>
+#include "SDL3/SDL_stdinc.h"
+#include "shader.h"
+#include "float3.h"
+#include "float4.h"
+#include "mat4x4.h"
+#include <stddef.h>
+
+extern "C" {
+	//resources
+	struct xxx_buffer
+	{
+
+	};
+
+	struct xxx_shader
+	{
+
+	};
+
+	struct xxx_program
+	{
+		unsigned int id;
+	};
+
+	struct xxx_graphicsPipeline
+	{
+		xxx_program program;
+		
+	};
+
+	struct xxx_renderer
+	{
+		SDL_GLContext glContext;
+		xxx_graphicsPipeline rectPipeline;
+		xxx_graphicsPipeline circlePipeline;
+		xxx_graphicsPipeline spritePipeline;
+	};
+}
+
+struct ConstantTexture
+{
+	GLuint value;
+};
+
+struct ConstantTextureLookupItem
+{
+	const char* key;
+	ConstantTexture texture;
+};
+
+struct ConstantTextureLookup
+{
+	ConstantTextureLookupItem* items;
+	size_t capacity;
+};
+
+static unsigned long long ConstantTextureLookupHash(const char* str)
+{
+	unsigned long long hash = 5381;
+	while (*str)
+	{
+		char c = *str++;
+		hash = ((hash << 5) + hash) + (unsigned char)c;
+	}
+
+	return hash;
+}
+
+ConstantTextureLookup ConstantTextureLookupCreate(size_t capacity)
+{
+	ConstantTextureLookup lookup;
+	lookup.items = (ConstantTextureLookupItem*)malloc(sizeof(*lookup.items) * capacity);
+	lookup.capacity = capacity;
+	memset(lookup.items, 0, sizeof(*lookup.items) * capacity);
+	return lookup;
+}
+
+void ConstantTextureLookupAdd(ConstantTextureLookup* lookup, const char* key, ConstantTexture* texture)
+{
+	unsigned long long slot = ConstantTextureLookupHash(key) % lookup->capacity;
+	for (size_t index = 0; index < lookup->capacity; index++)
+	{
+		size_t at = (slot + index) % lookup->capacity;
+		ConstantTextureLookupItem* item = &lookup->items[at];
+		if (item->key == nullptr)
+		{
+			item->key = key;
+			item->texture.value = texture->value;
+			return;
+		}
+		int result = strcmp(item->key, key);
+		if (result == 0)
+		{
+			return;
+		}
+		// Collision, result != 0 -> different key stored already, continue
+	}
+}
+
+ConstantTexture* ConstantTextureLookupGet(ConstantTextureLookup* lookup, const char* key)
+{
+	unsigned long long slot = ConstantTextureLookupHash(key) % lookup->capacity;
+	for (size_t index = 0; index < lookup->capacity; index++)
+	{
+		size_t at = (slot + index) % lookup->capacity;
+		ConstantTextureLookupItem* item = &lookup->items[at];
+		if (item->key == nullptr)
+		{
+			return nullptr;
+		}
+		int result = strcmp(item->key, key);
+		if (result == 0)
+		{
+			return &item->texture;
+		}
+	}
+	// Increase load factor pls
+	return nullptr;
+}
+
+ConstantTextureLookup textureLookup = ConstantTextureLookupCreate(128);
+
+void GlobalTextureAdd(const char* key, ConstantTexture* texture)
+{
+	ConstantTextureLookupAdd(&textureLookup, key, texture);
+}
+
+ConstantTexture* GlobalTextureGet(const char* key)
+{
+	return ConstantTextureLookupGet(&textureLookup, key);
+}
+
+static void GetSimple2dShader()
+{
+	//attributes: position, color, texture coords
+	float vertices[] = {
+		// positions          // colors           // texture coords
+		 0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right
+		 0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom right
+		-0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
+		-0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left 
+	};
+	
+	unsigned int indices[] =  // note that we start from 0!
+	{
+		0, 1, 3,   // first triangle
+		1, 2, 3    // second triangle
+	};
+
+	//GL_STREAM_DRAW: the data is set only once and used by the GPU at most a few times.
+	//GL_STATIC_DRAW : the data is set only once and used many times.
+	//GL_DYNAMIC_DRAW : the data is changed a lot and used many times.
+
+	//VAO
+	unsigned int VAO;
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+
+	//VBO
+	unsigned int VBO;
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	//EBO
+	unsigned int EBO;
+	glGenBuffers(1, &EBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	//Set vertex attributes pointers
+
+	// position attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	// color attribute
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+
+	// texture coord attribute
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+	glEnableVertexAttribArray(2);
+}
+
+unsigned int loadImage(const char* path)
+{
+	SDL_Surface* surface = IMG_Load(path);
+	if (!surface)
+	{
+		std::cerr << "Failed to load image: " << std::endl;
+		return 0;
+	}
+
+	SDL_FlipSurface(surface, SDL_FLIP_VERTICAL);
+
+	GLuint texture;
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, surface->w, surface->h, 0, GL_RGBA, GL_UNSIGNED_BYTE, surface->pixels);
+	glGenerateMipmap(GL_TEXTURE_2D);
+
+	SDL_DestroySurface(surface);
+
+	ConstantTexture constantTexture;
+	constantTexture.value = texture;
+	GlobalTextureAdd(path, &constantTexture);
+
+	return texture;
+}
+
+void bindTexture(unsigned int texture)
+{
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, texture);
+}
+
+void bindTexture(const char* key)
+{
+	ConstantTexture* texture = GlobalTextureGet(key);
+
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, texture->value);
+}
 
 void checkCompileErrors(unsigned int shader, std::string type)
 {
@@ -72,6 +308,189 @@ unsigned int createProgram(unsigned int shaders[], int count)
 	return shaderProgram;
 }
 
+static void CompileAndLinkShaders(unsigned int& shaderProgram, const char* vertexShaderSource, const char* fragmentShaderSource)
+{
+	//Create and compile shaders
+	unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER); //create shadre of type
+	compileShader(vertexShader, vertexShaderSource);
+
+	unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	compileShader(fragmentShader, fragmentShaderSource);
+
+	//Create and link shader programs
+	unsigned int shaders[] = { vertexShader, fragmentShader }; //these can be specified
+
+	shaderProgram = createProgram(shaders, SDL_arraysize(shaders));
+}
+
+unsigned int createShader(const char* vertexPath, const char* fragmentPath)
+{
+	//todo is this correct?
+    std::ifstream vertexFile;
+    std::ifstream shaderFile;
+  
+    vertexFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+    shaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+ 
+	const char* vertexCode;
+    const char* fragmentCode;
+
+	if(vertexPath)
+	{
+		std::string vertexStream;
+		vertexFile.open(vertexPath);
+
+		std::stringstream vShaderStream;
+		vShaderStream << vertexFile.rdbuf();
+
+		vertexFile.close();
+
+		vertexStream = vShaderStream.str();
+		vertexCode = vertexStream.c_str();
+	}
+        
+	if(fragmentPath)
+	{
+		std::string fragmentStream;
+        shaderFile.open(fragmentPath);
+
+        std::stringstream fShaderStream;  
+        fShaderStream << shaderFile.rdbuf();
+      
+        shaderFile.close();
+     
+		fragmentStream = fShaderStream.str();
+		fragmentCode = fragmentStream.c_str();
+	}
+
+	unsigned int ID;
+	CompileAndLinkShaders(ID, vertexCode, fragmentCode);
+	return ID;
+}
+
+void createPipeline(xxx_graphicsPipeline* pipeline, const char* vertexPath = nullptr, const char* fragmentPath = nullptr)
+{
+	bool wirefame = false;
+
+	if( vertexPath )
+	{
+		bool vsExists = std::filesystem::exists(vertexPath);
+		SDL_assert(vsExists && "Shader file does not exist");
+	}
+	
+	if( fragmentPath )
+	{
+		bool fExists = std::filesystem::exists(fragmentPath);
+		SDL_assert(fExists && "Shader file does not exist");
+	}
+
+	pipeline->program.id = createShader(vertexPath, fragmentPath);
+
+	GetSimple2dShader();
+
+	unsigned int texutre = loadImage(GL_RESOURCE_DIRECTORY_PATH"/assets/textures/demon.png");
+}
+
+struct xxx_renderer* xxx_rendererCreate(SDL_Window* window)
+{
+	xxx_renderer* renderer = (xxx_renderer*)SDL_malloc(sizeof(*renderer));
+
+	// Set OpenGL attributes
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 4);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+
+	// Create an OpenGL context
+	renderer->glContext = SDL_GL_CreateContext(window);
+
+	if (renderer->glContext == nullptr)
+	{
+		std::cerr << "SDL_GL_CreateContext Error: " << SDL_GetError() << std::endl;
+		SDL_Quit();
+		return nullptr;
+	}
+
+	// Initialise GLAD
+	if (!gladLoadGLLoader((GLADloadproc)SDL_GL_GetProcAddress))
+	{
+		std::cerr << "Failed to initialize GLAD" << std::endl;
+		SDL_Quit();
+		return nullptr;
+	}
+	int windowWidth, windowHeight;
+	SDL_GetWindowSize(window, &windowWidth, &windowHeight);
+
+	glViewport(0, 0, windowWidth, windowHeight);
+
+	createPipeline(&renderer->rectPipeline, GL_RESOURCE_DIRECTORY_PATH"/shaders/learning/triangle.vs", GL_RESOURCE_DIRECTORY_PATH"/shaders/learning/triangle.fs");
+	createPipeline(&renderer->circlePipeline);
+	createPipeline(&renderer->spritePipeline);
+
+	return renderer;
+}
+
+void xxx_rendererDestroy(xxx_renderer* renderer)
+{
+	SDL_GL_DestroyContext(renderer->glContext);
+	SDL_free(renderer);
+};
+
+xxx_api* xxx_load()
+{
+	return nullptr;
+}
+
+void use(xxx_program* program)
+{
+	glUseProgram(program->id);
+}
+
+void setBool( xxx_program* program, const std::string& name, bool value )
+{
+	glUniform1i(glGetUniformLocation(program->id, name.c_str()), (int)value);
+}
+
+void setInt(xxx_program* program, const std::string& name, int value )
+{
+	glUniform1i(glGetUniformLocation(program->id, name.c_str()), value);
+}
+
+void setFloat(xxx_program* program, const std::string& name, float value )
+{
+	glUniform1f(glGetUniformLocation(program->id, name.c_str()), value);
+}
+
+static void DrawSprite(xxx_program* program, unsigned int texture)
+{
+	bindTexture(texture);
+
+	//Use shader program when rendering
+	use(program);
+
+	setInt(program, "texture", 1);
+
+	mat4x4 trans = mat4x4(1.0f);
+	//trans = translate(trans, float3(1.0f, 0.0f, 0.0f));
+	//trans = scale(trans, float3(0.5, 0.5, 0.5));
+	//trans = rotationZ(trans, 0.5); 
+	//print(trans);
+
+	unsigned int transformLoc = glGetUniformLocation(program->id, "transform");
+	glUniformMatrix4fv(transformLoc, 1, GL_FALSE, trans.elements);
+
+	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+	//glDrawArrays(GL_TRIANGLES, 0, 3);
+	
+	//unbinds?
+	//vao unbnd before ebo
+	//glDeleteVertexArrays(1, &VAO);
+	//glDeleteBuffers(1, &VBO);
+	//glDeleteBuffers(1, &EBO);
+	//glDeleteProgram(shaderProgram);
+}
+
+/*
 enum class MaterialEditorShaderType
 {
 	Vertex,
@@ -109,74 +528,4 @@ struct MaterialEditor
 	const char* path;
 	MaterialEditorParameters parameters;
 };
-
-static void CompileAndLinkShaders(unsigned int& shaderProgram, const char* vertexShaderSource, const char* fragmentShaderSource)
-{
-	//Create and compile shaders
-	unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER); //create shadre of type
-	compileShader(vertexShader, vertexShaderSource);
-
-	unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	compileShader(fragmentShader, fragmentShaderSource);
-
-	//Create and link shader programs
-	unsigned int shaders[] = { vertexShader, fragmentShader }; //these can be specified
-
-	shaderProgram = createProgram(shaders, SDL_arraysize(shaders));
-}
-
-Shader::Shader(const char* vertexPath, const char* fragmentPath)
-{
-    std::string vertexStream;
-    std::string fragmentStream;
-    std::ifstream vertexFile;
-    std::ifstream shaderFile;
-  
-    vertexFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
-    shaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
-    try //todo how to make this not try catch?
-    {
-        vertexFile.open(vertexPath);
-        shaderFile.open(fragmentPath);
-        std::stringstream vShaderStream, fShaderStream;
-       
-        vShaderStream << vertexFile.rdbuf();
-        fShaderStream << shaderFile.rdbuf();
-      
-        vertexFile.close();
-        shaderFile.close();
-     
-		vertexStream = vShaderStream.str();
-		fragmentStream = fShaderStream.str();
-    }
-    catch (std::ifstream::failure e)
-    {
-        std::cout << "ERROR::SHADER::FILE_NOT_SUCCESFULLY_READ" << std::endl;
-    }
-    const char* vertexCode = vertexStream.c_str();
-    const char* fragmentCode = fragmentStream.c_str();
-
-	CompileAndLinkShaders(ID, vertexCode, fragmentCode);
-}
-
-void Shader::use()
-{
-	glUseProgram(ID);
-}
-
-void Shader::setBool( const std::string& name, bool value ) const
-{
-	glUniform1i(glGetUniformLocation(ID, name.c_str()), (int)value);
-}
-
-void Shader::setInt( const std::string& name, int value ) const
-{
-	glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
-}
-
-void Shader::setFloat( const std::string& name, float value ) const
-{
-	glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
-}
-
-//todo set float3
+*/
--- src/shader.h
+++ src/shader.h
@@ -1,26 +1,41 @@
 #ifndef SHADER_H
 #define SHADER_H
 
-#include <glad/glad.h> // include glad to get all the required OpenGL headers
+#include <stddef.h>
 
-#include <string>
-#include <fstream>
-#include <sstream>
-#include <iostream>
+extern "C" {
+    enum xxx_shader_type
+	{
+		XXX_SHADER_TYPE_VERTEX = 0,
+		XXX_SHADER_TYPE_FRAGMENT = 1,
+	};
 
-class Shader
-{
-public:
-    unsigned int ID;
+    struct xxx_bufferApi
+	{
+		void(*destroy)(struct xxx_bufferApi*);
+	};
 
-    // constructor reads and builds the shader
-    Shader(const char* vertexPath, const char* fragmentPath);
-  
-    void use();
+	struct xxx_shaderApi
+	{
+		struct xxx_shader* (*create)(xxx_shader_type type, char* source, size_t size);
+		void(*destroy)(struct xxx_shader*); //in this one delete program
+	};
 
-    void setBool(const std::string& name, bool value) const;
-    void setInt(const std::string& name, int value) const;
-    void setFloat(const std::string& name, float value) const;
-};
+	struct xxx_programApi
+	{
+		struct xxx_program* (*create)(void);
+		void (*destroy)(struct xxx_program* program);
+	};
+
+	struct xxx_api
+	{
+		struct xxx_renderer* (*create)(void);
+		void(*destroy)(struct xxx_renderer*);
+		struct xxx_shaderApi* shader;
+		struct xxx_programApi* program;
+	};
+
+	struct xxx_api* xxx_load();
+}
 
 #endif
